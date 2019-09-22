ef236fc38831c5d0d3fdfe5174e44a22217e40ed
王开放 <wangkaifang@baidu.com>
2019-09-22 23:41:01 +0800

    update vue

---
+++ webapp/src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h()
+}).$mount("#app");
